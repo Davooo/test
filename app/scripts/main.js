e591dc34f9c5919625b5c89ae66bc8c6ca8622f9
unknown <gabrieledavid38@gmail.com>
2014-07-28 23:26:50 +0200

    update 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+	console.log($(".openbutton"));
+	$(".openbutton").on("click",affichemenu);
+	$("li").on("click",changecolor);
+})
+
+function affichemenu() {
+	if ($("nav").hasClass("inactive")){
+		$("nav").removeClass("inactive");	
+
+	}
+	else{
+		$("nav").addClass("inactive");
+		$("body").css("background","white");
+
+		}
+}
+
+function changecolor( event ) {
+	console.log(event);
+	var color = $(event.currentTarget).attr("data-color");
+	$("body").css("background",color);
+}
+
+
