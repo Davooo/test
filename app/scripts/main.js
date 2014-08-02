4cd7e4b6ade45a1119400b1e8834c2d1f8382be4
unknown <gabrieledavid38@gmail.com>
2014-08-02 14:15:05 +0200

    ep

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,35 @@
 console.log('\'Allo \'Allo!');
 
+var pageid = null;
+
 $(document).ready(function(){
 	console.log($(".openbutton"));
 	$(".openbutton").on("click",affichemenu);
 	$("li").on("click",changecolor);
-		$("li").on("click",affcihep1);
-
-
+	$("li").on("click",changepage);
 })
 
 function affichemenu() {
 	if ($("nav").hasClass("inactive")){
-		$("nav").removeClass("inactive");	
-
+		$("nav").removeClass("inactive");
 	}
 	else{
 		$("nav").addClass("inactive");
 		$("body").css("background","white");
-
-		}
+		$("div#"+pageid).removeClass("show");
+		pageid = null;
+	}
 }
 
 function changecolor( event ) {
-	console.log(event);
+	// console.log(event);
 	var color = $(event.currentTarget).attr("data-color");
 	$("body").css("background",color);
 }
-
-
-
-
-
+function changepage ( event) {
+	if ( pageid != null){
+		$("div#"+pageid).removeClass("show");
+	}
+	pageid = $(event.currentTarget).attr("data-pageid");
+	$("div#"+pageid).addClass("show");
+}
